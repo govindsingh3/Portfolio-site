eed3dbd1ccd0a613ed861c86361da665c41e4a34
Govind Pratap Singh <govindsunbeam@gmail.com>
2025-07-29 00:09:06 +0530

    final

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+static_dir = os.path.join(os.path.dirname(__file__), 'static')
+
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Required by Vercel
 def handler(environ, start_response):
     return app.wsgi_app(environ, start_response)
