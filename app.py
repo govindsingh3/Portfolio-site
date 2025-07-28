592fc70481a6e26775797e7860420cbd224dcac9
Govind Pratap Singh <govindsunbeam@gmail.com>
2025-07-29 00:01:58 +0530

    final

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def handler(environ, start_response):
+    return app.wsgi_app(environ, start_response)
